a1af7f98584e4536c4e685733af3da975e55bde8
Chris Magnuson <hourglasssand@gmail.com>
2017-03-23 15:22:32 -0400

    Initial commit

---
+++ TervisServerManager.psm1
@@ -0,0 +1,33 @@
+﻿function Install-TervisWindowsFeature {
+    [CmdletBinding()]
+    param (
+        $ComputerName,
+        $Credential = [System.Management.Automation.PSCredential]::Empty,
+        $WindowsFeatureGroupNames
+    )    
+    $WindowsFeatureGroups = $WindowsFeatureGroupNames | Get-WindowsFeatureGroup
+    $WindowsFeatures = $WindowsFeatureGroups.WindowsFeature | sort -Unique
+    
+    if ($Credential-ne [System.Management.Automation.PSCredential]::Empty) {
+        Install-WindowsFeature -Name $WindowsFeatures -ComputerName:$ComputerName -Credential:$Credential
+    } else {
+        Install-WindowsFeature -Name $WindowsFeatures -ComputerName:$ComputerName
+    }
+}
+
+function Get-WindowsFeatureGroup {
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]$Name
+    )
+    process {
+        $WindowsFeatureGroups | where Name -eq $Name
+    }
+}
+
+$WindowsFeatureGroups = [PSCustomObject][Ordered] @{
+    Name = "BartenderCommander"
+    WindowsFeature = @"
+NET-Framework-Features
+NET-Framework-Core
+"@ -split "`r`n" 
+}
