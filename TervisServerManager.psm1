abd507c43daa2514d7ac15d9e41eec90f9eb4826
Chris Magnuson <hourglasssand@gmail.com>
2017-04-05 16:51:31 -0400

    Add Progistics

--- TervisServerManager.psm1
+++ TervisServerManager.psm1
@@ -30,4 +30,32 @@ $WindowsFeatureGroups = [PSCustomObject][Ordered] @{
 NET-Framework-Features
 NET-Framework-Core
 "@ -split "`r`n" 
+},
+[PSCustomObject][Ordered] @{
+    Name = "Progistics"
+    WindowsFeature = @"
+Web-Server
+Web-WebServer
+Web-Common-Http
+Web-Default-Doc
+Web-Dir-Browsing
+Web-Http-Errors
+Web-Static-Content
+Web-Health
+Web-Http-Logging
+Web-Performance
+Web-Stat-Compression
+Web-Security
+Web-Filtering
+Web-App-Dev
+Web-ASP
+Web-ISAPI-Ext
+Web-Mgmt-Tools
+Web-Mgmt-Console
+Web-Scripting-Tools
+NET-Framework-45-Features
+NET-Framework-45-Core
+NET-WCF-Services45
+NET-WCF-TCP-PortSharing45
+"@ -split "`r`n" 
 }
